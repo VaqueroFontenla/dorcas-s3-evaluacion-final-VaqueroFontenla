50ba506e1ed1cbd932fa2c3df42e79d04668684a
Loreto Vaquero <loret.vaquero@gmail.com>
2018-08-09 00:24:02 +0200

    working router

---
+++ src/components/CharacterDetail.js
@@ -0,0 +1,20 @@
+import React, { Component } from 'react';
+import HarryCard from './HarryCard';
+import './CharacterDetail.css';
+
+
+class CharacterDetail extends Component {
+  constructor (props) {
+    super (props)
+
+  }
+
+  render () {
+
+    return (
+      <div></div>
+    );
+  }
+}
+
+export default CharacterDetail;
